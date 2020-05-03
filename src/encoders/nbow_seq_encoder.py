c5343c64089d0c0518bdd732a8614b7f6036de13
Sujay Lokesh <sl5202@nyu.edu>
2020-05-03 08:34:10 -0400

    original

--- src/encoders/nbow_seq_encoder.py
+++ src/encoders/nbow_seq_encoder.py
@@ -9,7 +9,7 @@ from utils.tfutils import pool_sequence_embedding
 class NBoWEncoder(MaskedSeqEncoder):
     @classmethod
     def get_default_hyperparameters(cls) -> Dict[str, Any]:
-        encoder_hypers = { 'nbow_pool_mode': 'concat',
+        encoder_hypers = { 'nbow_pool_mode': 'weighted_mean',
                          }
         hypers = super().get_default_hyperparameters()
         hypers.update(encoder_hypers)
